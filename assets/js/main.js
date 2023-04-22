ec1e8ce97258d519ff7063bbb294bbd8280a34d6
Darccelio Silva <darccelio@gmail.com>
2023-04-22 19:09:12 -0300

    consuming data from the pokeapi api and presenting the result in a paginated way

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,44 @@
-console.log('Hello Word')
+const limit = 5
+let offset = 0;
+
+const pokemonsList = document.querySelector('.pokemons')
+const btnLoadMorePokemons = document.querySelector('#loadMoreButton')
+btnLoadMorePokemons.addEventListener('click', loadMorePokemon)
+loadMorePokemon()
+
+function loadMorePokemon() {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    if (pokemons.length > 0) {
+      offset += limit
+      pokemonsList.innerHTML += pokemons.map(convertPokemonToListHtml).join('')
+    }
+  })
+}
+
+function convertPokemonToListHtml(pokemon) {
+  return `
+  <li class="pokemon ${pokemon.principalType}">
+
+    <span class="number">#${pokemon.number}</span>
+    <span class="name">${pokemon.name}</span>
+
+    <div class="detail">
+      <ol class="types">
+        ${pokemon.types
+          .map(type => `<li class="type ${type}">${type}</li>`)
+          .join('')}
+      
+      </ol>
+      <img
+        src="${pokemon.picture}"
+        alt="${pokemon.name}"
+      />
+    </div>
+
+  </li>
+  `
+}
+
+
+
+
---
+++ assets/js/poke-api.js
@@ -0,0 +1,40 @@
+const pokeApi = {};
+
+function convertPokeApiDetailToPokemon(pokeDetail) { //conversão do modelo da api pokemon para o objeto Pokemon
+  const pokemon = new Pokemon()
+  pokemon.name = pokeDetail.name
+  pokemon.number = pokeDetail.id
+  const types = pokeDetail.types.map(typesSlot => typesSlot.type.name)
+  const [type] = types //destruturação do objeto, colocando na primeira posição de um array
+  pokemon.types = types
+  pokemon.principalType = type
+  pokemon.picture = pokeDetail.sprites.other.dream_world.front_default
+  return pokemon
+}
+
+pokeApi.getPokemonDetail = pokemon => {
+  return (
+    fetch(pokemon.url)
+      .then(response => response.json()) // é realizado a requisição na propriedade url
+      // de detalhes do pokemon e convertido em objeto json e retornado esse objeto para o map que o chamou.
+      .then(convertPokeApiDetailToPokemon)
+  )
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 100) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+  return (
+    fetch(url)
+      .then(res => res.json()) // pega o objeto http response e converte para objeto json
+      .then(jsonBody => jsonBody.results) // pega o objeto json e converte em lista de pokemons
+      .then(pokemons => pokemons.map(pokeApi.getPokemonDetail)) // através do map, para cada pokemon da lista de pokemons,
+      // é enviado por referência um objeto pokemon para o método getPokemonDetail
+      // (para que seja feito uma requisição para cada pokemon) a fim de obter uma nova lista objetos json com os detalhes de cada pokemon
+      //é retornado uma lista de Promessas (pendentes)
+      .then(requestsDetail => Promise.all(requestsDetail)) // a partir das requisições de detalhes assincronas de pokemons
+      // (promessas pendentes de serem resolvidas),é aguardado através do Promise.all até que todas as requisições sejam resolvidas,
+      // obtendo uma lista de detalhes do pokemon em formato de lista
+      .then(pokemonsDetails => pokemonsDetails)
+  ) // lista de detalhes de pokemon resolvida
+}
